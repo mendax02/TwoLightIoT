fb26582ab7fd79193741fda712e0bc084c7dd324
mendax02 <siddharth.rawat@outlook.com>
2019-03-31 10:43:20 +0530

    Add project files.

---
+++ TwoLightIoT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoLightIoT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
