37e3c5c6840ec95c02a9c74210c70d92ced34d57
mendax02 <siddharth.rawat@outlook.com>
2019-03-31 13:50:07 +0530

    Alternate light blink using .net core and raspberryPie

--- TwoLightIoT/Program.cs
+++ TwoLightIoT/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Gpio;
 
 namespace TwoLightIoT
 {
@@ -6,7 +9,31 @@ namespace TwoLightIoT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var blinkingPinGreen = Pi.Gpio[P1.Gpio22];// Physical pin 15
+            var blinkingPinRed = Pi.Gpio[P1.Gpio27];// Physical pin 13
+            blinkingPinGreen.PinMode = GpioPinDriveMode.Output;
+            blinkingPinRed.PinMode = GpioPinDriveMode.Output;
+
+            for (byte i = 0; i < 40; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    blinkingPinRed.Write(false);
+                    Thread.Sleep(500);
+                    blinkingPinGreen.Write(true);
+
+                }
+                else
+                {
+                    blinkingPinGreen.Write(false);
+                    Thread.Sleep(500);
+                    blinkingPinRed.Write(true);
+                }
+                Thread.Sleep(2500);
+            }
+            blinkingPinGreen.Write(false);
+            blinkingPinRed.Write(false);
+            Environment.Exit(0);
         }
     }
 }
